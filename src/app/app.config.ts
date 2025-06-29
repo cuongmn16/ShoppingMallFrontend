2aab29a0beb8532d3c27ec81f6c9fc66ed328c6c
DucHoang0210 <duchoang9.it@gmail.com>
2025-06-29 18:01:35 +0700

    cart, auth to make cart

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,22 @@
-import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
-import {provideAnimations} from '@angular/platform-browser/animations';
-import {provideToastr} from 'ngx-toastr';
+import { AuthInterceptor } from './services/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
     provideToastr(),
-    importProvidersFrom(HttpClientModule),
-    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(
+      withInterceptors([AuthInterceptor])
+    ),
+  ],
 };
---
+++ src/app/services/auth.interceptor.ts
@@ -0,0 +1,20 @@
+// src/app/auth.interceptor.ts
+import { inject } from '@angular/core';
+import { HttpInterceptorFn } from '@angular/common/http';
+import { AuthService } from '../services/auth.service';
+
+export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const token = authService.getToken(); // bạn sẽ viết getToken() trong AuthService
+
+  if (token) {
+    const cloned = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return next(cloned);
+  }
+
+  return next(req);
+};
